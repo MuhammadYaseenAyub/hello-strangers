06f7065bd21ff1ac138dd69a5fd8449616ab6a16
alisrepository <49435870+alisrepository@users.noreply.github.com>
2021-11-23 22:07:19 +0500

    geo location added
    still need to fix the scroll functionality

--- app/Views/hello.php
+++ app/Views/hello.php
@@ -98,13 +98,49 @@ ul li:nth-child(2n-2){
         <input type="text" name="message" id="" placeholder="message">
         <button type="submit" id="submit-btn">Submit</button>
     </form>
+    <button type='button' id='send-location'>Send location</button>
 </body>
+
+<script id='message-template' type='text/template'>
+    <li class='message'>
+        <div class='message__title'>
+            <h4>
+                @{{from}}
+            </h4>
+            <span>@{{createdAt}}</span>
+        </div>
+        <div class='message__body'>
+            <p>@{{text}}</p>
+        </div>
+    </li>
+</script>
+
+<script id='loc-message-template' type='text/template'>
+    <li class='message'>
+        <div class='message__title'>
+            <h4>
+                @{{from}}
+            </h4>
+            <span>@{{createdAt}}</span>
+        </div>
+        <div class='message__body'>
+            <a href="@{{url}}" target='_blank'>Location</a>
+        </div>
+    </li>
+</script>
+
 <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 <script src="https://cdn.socket.io/4.3.2/socket.io.min.js" integrity="sha384-KAZ4DtjNhLChOB/hxXuKqhMLYvx3b5MlT55xPEiNmREKRzeEm+RVPlTnAn0ajQNs" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.1/moment.min.js" integrity="sha512-qTXRIMyZIFb8iQcfjXWCO8+M5Tbc38Qi5WzdPOYZHIlZpzBHG3L3by84BBBOiRGiEb7KKtAOAs5qYdUiZiQNNQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/mustache.js/0.1/mustache.min.js" integrity="sha512-3GRj7sme01zpWwRNPNy48Rda1bD9cq34lqYG5vb8ZXGc+wRqsoBJ3+AC25IYW5w5SrWlzHqIpNIxZt5QF9sXLg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
 <script>
+    function scrollToBottom(){
+        let msgs = template = document.querySelector('body').lastElementChild;
+        msgs.scrollIntoView();
+    }
   var socket = io('http://localhost:3000/');
   socket.on('connect', function(){
       console.log("connected to server....");
@@ -119,11 +155,46 @@ ul li:nth-child(2n-2){
   });
 
   socket.on('newMsg', function(message){
-      console.log('New msg:> ', message);
+    const datef = moment(message.createdAt).format('LT');
+      const template = document.querySelector('#message-template').innerHTML;
+      const html = Mustache.render(template, {
+          from: message.from,
+          text: message.text,
+          createdAt: datef
+      });
+      const div = document.createElement('div');
+      div.innerHTML = html;
+      document.querySelector('body').appendChild(div);
+      scrollToBottom();
+      /*console.log('New msg:> ', message);
+      const datef = moment(message.createdAt).format('LT');
       let li = document.createElement('li');
-      li.innerText = message.from+" "+message.text;
+      li.innerText = message.from+" "+datef+" :"+message.text;
 
-      document.querySelector('body').appendChild(li);
+      document.querySelector('body').appendChild(li);*/
+  });
+
+  socket.on('newLocMsg', function(message){
+    const datef = moment(message.createdAt).format('LT');
+    console.log('newLocMsg:> ', message);
+    const template = document.querySelector('#loc-message-template').innerHTML;
+      const html = Mustache.render(template, {
+          from: message.from,
+          url: message.url,
+          createdAt: datef
+      });
+      const div = document.createElement('div');
+      div.innerHTML = html;
+      document.querySelector('body').appendChild(div);
+      scrollToBottom();
+      /*let li = document.createElement('li');
+      let a = document.createElement('a');
+      a.setAttribute('target', '_blank');
+      console.log(message.url);
+      a.setAttribute('href', message.url);
+      a.innerText = "My location";
+      li.appendChild(a);
+      document.querySelector('body').appendChild(li);*/
   });
 
   /*socket.emit('createMsg', {
@@ -140,6 +211,22 @@ ul li:nth-child(2n-2){
         text: document.querySelector('input[name="message"]').value
     }, function(){
 
+    });
+    document.querySelector('input[name="message"]').value="";
+  })
+
+  document.querySelector('#send-location').addEventListener('click', function(e){
+    if(!navigator.geolocation){
+        return alert("Geo-Location not supported by your browser.");
+    }
+    navigator.geolocation.getCurrentPosition(function (position){
+        console.log(position);
+        socket.emit('createLocMsg', {
+            lat:position.coords.latitude,
+            long:position.coords.longitude,
+        });
+    }, function(){
+        alert("unable to get location.");
     });
   })
 </script>
